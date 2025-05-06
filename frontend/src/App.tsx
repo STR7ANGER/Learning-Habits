728db703280d9f94bf916a70df1963dfbec21898
Aditya <adityamaurya.2807@gmail.com>
2025-05-06 23:23:43 +0530

    help

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,17 @@ import ClickSpark from "./blocks/Animations/ClickSpark/ClickSpark";
 
 const App = () => {
   return (
-    <>
-      <ClickSpark
-        sparkColor="#000000"
-        sparkSize={10}
-        sparkRadius={15}
-        sparkCount={8}
-        duration={400}
-      >
-        <AuthProvider>
-          <AppRoutes />
-        </AuthProvider>
-      </ClickSpark>
-    </>
+    <ClickSpark
+      sparkColor="#000000"
+      sparkSize={10}
+      sparkRadius={15}
+      sparkCount={8}
+      duration={400}
+    >
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
+    </ClickSpark>
   );
 };
 
